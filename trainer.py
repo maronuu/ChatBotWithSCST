ee86d14a6a4d2f1fc26572ff79342ca689441628
maronuu <oguri.ytr.2000@gmail.com>
2021-03-22 13:37:58 +0900

    make trainer class for REINFORCE algorithm

--- trainer.py
+++ trainer.py
@@ -10,7 +10,7 @@ from tensorboardX import SummaryWriter
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
-import ptan
+from ptan.common.utils import TBMeanTracker
 
 from . import utils, model, data
 
@@ -185,15 +185,15 @@ class TrainerCrossEntropy:
 
 
 
-
 class TrainerReinforce:
-    def __init__(self, genre, use_cuda, name_of_run, log, model, model_path, batch_size, lr, n_epochs):
+    def __init__(self, genre, use_cuda, name_of_run, log, model, model_path, n_samples, batch_size, lr, n_epochs):
         is_cuda_available = torch.cuda.is_available()
         if (not is_cuda_available) and use_cuda:
             raise RuntimeError("GPU is not available, but you tried to use it.")
         self.use_cuda = use_cuda
         self.log = log
         self.n_epochs = n_epochs
+        self.n_samples = n_samples
         self.batch_size = batch_size
 
         self.random_state = np.random.RandomState(data.SEED)
@@ -258,10 +258,15 @@ class TrainerReinforce:
         
         return total_bleu / num_bleu
     
-    def _write(self, epoch, loss, bleu_score_train, bleu_score_test):
-        self.writer.add_scalar('loss', loss, epoch)
-        self.writer.add_scalar('bleu_train', bleu_score_train, epoch)
-        self.writer.add_scalar('bleu_test', bleu_score_test, epoch)
+    def _write(self, batch_idx, epoch, bleu_score_train, bleu_score_test, mean_bleu_score_sampling, num_total_samples, num_skipped_samples):
+        self.writer.add_scalar("bleu_test", bleu_score_test, batch_idx)
+        self.writer.add_scalar("bleu_argmax", bleu_score_train, batch_idx)
+        self.writer.add_scalar("bleu_sampling",
+                            mean_bleu_score_sampling, batch_idx)
+        self.writer.add_scalar("skipped_samples",
+                            num_skipped_samples / num_total_samples,
+                            batch_idx)
+        self.writer.add_scalar("epoch", batch_idx, epoch)
 
     def _plot_and_save_fig(self, yy, ylabel_name, fig_name, save_dir=IMAGE_DIR):
         if not os.path.isdir('./images'):
@@ -278,10 +283,13 @@ class TrainerReinforce:
         # log format
         fmt = "%(asctime)-15s %(levelname)s %(message)s"
         logging.basicConfig(format=fmt, level=logging.INFO)
-        # temporary best score
-        best_score = None
         # writer
         self.writer = SummaryWriter(comment='-' + self.name_of_run)
+        eof = self.emb_dict[data.END]
+
+        
+        # temporary best score
+        best_score = None
         # optimizer
         self.optimizer = optim.Adam(self.net.parameters(), lr=self.lr)
         # record
@@ -289,66 +297,115 @@ class TrainerReinforce:
         bleu_train_record = []
         bleu_test_record = []
 
-        with
+        batch_idx = 0
 
         for epoch in range(self.n_epochs):
-            total_bleu = 0.
-            num_bleu = 0
+            ### EPOCH START
+            num_total_samples = 0
+            num_skipped_samples = 0
+            bleu_score_argmax_list = []
+            bleu_score_sampling_list = []
             loss_list = []
-            for batch in data.batch_iterator(self.train_data, self.batch_size):
-                packed_question_seq, packed_responce_seqs, questions, responces = data.pairs_to_packed_seq(batch, embedding=self.net.embedding, device=self.device)
 
+            random.shuffle(self.train_data)
+
+            for batch in data.batch_iterator(self.train_data, self.batch_size):
+                ### BATCH ITERATION START
+                batch_idx += 1
+                # packed form
+                packed_question_seq, questions, responces = data.pairs_to_packed_seq_without_out(batch, embedding=self.net.embedding, device=self.device)
                 # question_seq -> [Encoder] -> encoded
                 encoded = self.net.encode(packed_question_seq)
+                # start decoding from init_emb.
+                init_emb = self.net.embedding(self.begin_token)
 
-                output_list = list()  # output list
-                ref_list = list()  # reference(target) list
-                for i, responce_seq in enumerate(packed_responce_seqs):
-                    hidden = (encoded[j][:, i:i+1].contiguous() for j in [0, 1])
-                    ref_seq = responces[i][1:]  # omit first token.
-
-                    if random.random() < 0.5:
-                        # decode training data directly.
-                        out_g = self.net.decode_train_data(hidden, responce_seq)
-                        out = torch.max(out_g.data, dim=1)[1]
-                        out = out.cpu().numpy()
-                        total_bleu += utils.calc_bleu_score_for_seq(out, ref_seq)
-                    else:
-                        # decode and generate phrase.
-                        out_g, actions = self.net.decode_sequences(
-                            hidden=hidden,
-                            length=len(ref_seq),
-                            init_emb=responce_seq[0:1],
-                            mode='argmax'
-                        )
-                        total_bleu += utils.calc_bleu_score_for_seq(actions, ref_seq)
-                    
-                    output_list.append(out_g)
-                    ref_list += ref_seq
-                    num_bleu += 1
+                policie_list = []
+                action_list = []
+                advantage_list = []
                 
-                # concatenate all outputs from each batch.
-                out_tensor = torch.cat(output_list)
-                ref_tensor = torch.Tensor(ref_list).to(torch.long).to(self.device)
+                for i, question in enumerate(questions):
+                    ### ENCODING & DECODING START
+                    num_total_samples += 1
+                    hidden = (encoded[j][:, i:i+1].contiguous() for j in [0, 1])
+                    ref_seqs = [responce[1:] for responce in responces]  # omit first token.
+
+                    # act in detereminstic way
+                    logits_argmax, actions_argmax = self.net.decode_sequences(
+                        hidden=hidden,
+                        length=data.MAX_LENGTH,
+                        init_emb=init_emb,
+                        mode='argmax',
+                        end_of_decoding=eof,
+                    )
+                    bleu_score_argmax = utils.calc_bleu_score_for_seqs(actions_argmax, ref_seqs)
+                    bleu_score_argmax_list.append(bleu_score_argmax)
+
+                    if bleu_score_argmax > 0.98:
+                        num_skipped_samples += 1
+                        continue
+
+                    # act in probablistic way
+                    for _ in range(self.n_samples):
+                        logits_sampling, actions_sampling =\
+                            self.net.decode_sequences(
+                                hidden=hidden,
+                                length=data.MAX_LENGTH,
+                                init_emb=init_emb,
+                                mode='sampling',
+                                end_of_decoding=eof,
+                            )
+                        bleu_score_sampling = utils.calc_bleu_score_for_seqs(actions_sampling, ref_seqs)
+                        bleu_score_sampling_list.append(bleu_score_sampling)
+
+                        policie_list.append(logits_sampling)
+                        action_list += actions_sampling
+                        advantage = bleu_score_sampling - bleu_score_argmax
+                        # extend by action length
+                        advantage_list += [advantage] * (len(actions_sampling))
+                        ### ENCODING & DECODING END
+
+                if not policie_list:
+                    continue
+                    
+                policy_t = torch.cat(policie_list)  # on GPU
+                actions_t = torch.Tensor(action_list).to(torch.long).to(self.device)
+                advantage_t = torch.Tensor(advantage_list).to(torch.float).to(self.device)
+
+                # Loss
+                log_prob_t = F.log_softmax(policy_t, dim=1)
+                log_prob_a = log_prob_t[np.arange(len(action_list)), actions_t]
+                #  (Q(s) - b(s))log(p(a|s))
+                log_prob_a = advantage_t * log_prob_a
+                # -E_a[(Q(s) - b(s))log(p(a|s))]
+                loss = -log_prob_a.mean()
+                loss_list.append(loss.item())
+
                 # update
                 self.optimizer.zero_grad()
-                loss_g = F.cross_entropy(out_tensor, ref_tensor)
-                loss_g.backward()
+                loss.backward()
                 self.optimizer.step()
-                loss_list.append(loss_g.item())
-
-            # scores
-            loss = np.mean(loss_list)
-            bleu_score_train = total_bleu / num_bleu
+                ### BATCH ITERATION END
+            
+            # train, test, mean loss
+            bleu_score_train = np.mean(bleu_score_argmax_list)
             bleu_score_test = self.run_test()
-            loss_record.append(loss)
+            mean_loss = np.mean(loss_list)
             bleu_train_record.append(bleu_score_train)
             bleu_test_record.append(bleu_score_test)
             best_score = max(best_score, bleu_score_test)
-            self.log.info(f"Epoch {epoch} || mean loss: {loss:.5f} || mean BLEU score: {bleu_score_train:.5f} || test BLEU score: {bleu_score_test:.5f} || current best test BLEU score: {best_score:.5f}")
+            loss_record.append(mean_loss)
+            self.log.info(f"Epoch {epoch} || mean loss: {mean_loss:.5f} || mean BLEU score: {bleu_score_train:.5f} || test BLEU score: {bleu_score_test:.5f} || current best test BLEU score: {best_score:.5f}")
 
             # tensorboard
-            self._write(epoch, loss, bleu_score_train, bleu_score_test)
+            self._write(
+                batch_idx=batch_idx,
+                epoch=epoch,
+                bleu_score_train=bleu_score_train,
+                bleu_score_test=bleu_score_test,
+                mean_bleu_score_sampling=np.mean(bleu_score_sampling_list),
+                num_total_samples=num_total_samples,
+                num_skipped_samples=num_skipped_samples,
+            )
 
             # save
             if epoch % 5 == 0:
@@ -360,6 +417,6 @@ class TrainerReinforce:
 
         self.writer.close()
         # save record as figures.
-        self._plot_and_save_fig(loss_record, "Loss", "loss-train-cross-entropy")
-        self._plot_and_save_fig(bleu_train_record, "BLEU score on training", "bleu-train-cross-entropy")
-        self._plot_and_save_fig(bleu_test_record, "BLEU score on test", "bleu-test-cross-entropy")
+        self._plot_and_save_fig(loss_record, "Loss", "loss-train-reinforce")
+        self._plot_and_save_fig(bleu_train_record, "BLEU score on training", "bleu-train-reinforce")
+        self._plot_and_save_fig(bleu_test_record, "BLEU score on test", "bleu-test-reinforce")
